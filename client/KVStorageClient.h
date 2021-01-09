b646c1ab91dc54991f93fae194091178f3c885c0
Kirill Zheltovski <kirillzhelt@gmail.com>
2021-01-09 16:15:58 +0300

    Create TODOs

--- client/KVStorageClient.h
+++ client/KVStorageClient.h
@@ -7,13 +7,18 @@
 
 namespace KeyValueStorageCpp {
 
+	// TODO: change templating here
+	// KVStorageClient can be a cleint for different entities
+	// template just methods for CRUD
 	template <typename K, typename V>
 	class KVStorageClient {
 	public:
 		KVStorageClient(const std::string& host, int port);
 		~KVStorageClient();
 
+		// throws exception if keyspace already exists
 		void CreateKeySpace(const std::string& key_space);
+		// throws exception if keyspace does not exist
 		void DeleteKeySpace(const std::string& key_space);
 
 		std::vector<std::string> ListAllKeySpaces();
